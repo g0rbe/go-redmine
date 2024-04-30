29d38d6c63e812ab432f7cfa13c24ca8eb529d45
Daniel Gorbe <daniel@gorbe.io>
2024-04-30 22:51:12 +0200

    add examples

--- users_test.go
+++ users_test.go
@@ -1,1 +1,27 @@
 package redmine_test
+
+import (
+	"fmt"
+
+	"gorbe.io/go/redmine"
+)
+
+func ExampleUsers() {
+
+	users, err := redmine.NewPublic("https://www.redmine.org").Users()
+	if err != nil {
+		// handle error
+	}
+
+	fmt.Printf("%#v", users)
+}
+
+func ExampleUsers_withParams() {
+
+	users, err := redmine.NewPublic("https://www.redmine.org").Users(redmine.NameParameter("tester"))
+	if err != nil {
+		// handle error
+	}
+
+	fmt.Printf("%#v", users)
+}
