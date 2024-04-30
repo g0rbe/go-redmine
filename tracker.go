51a880ce87da22e7aa37cbc8b9ee46c7770bba59
Daniel Gorbe <daniel@gorbe.io>
2024-04-30 22:43:55 +0200

    implement Trackers API

--- tracker.go
+++ tracker.go
@@ -1,6 +1,37 @@
 package redmine
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 type Tracker struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
+	ID                    int      `json:"id"`
+	Name                  string   `json:"name"`
+	DefaultStatus         Status   `json:"default_status"`
+	Description           string   `json:"description"`
+	EnabledStandardFields []string `json:"enabled_standard_fields"`
+}
+
+func (r *Redmine) Trackers() ([]Tracker, error) {
+
+	code, body, err := r.auth.Request("GET", "/trackers.json", nil)
+	if err != nil {
+		return nil, fmt.Errorf("request failed: %w", err)
+	}
+
+	if code != 200 {
+		return nil, fmt.Errorf("%d %s", code, body)
+	}
+
+	v := struct {
+		Trackers []Tracker `json:"trackers"`
+	}{}
+
+	err = json.Unmarshal(body, &v)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal: %w", err)
+	}
+
+	return v.Trackers, nil
 }
---
+++ tracker_test.go
@@ -0,0 +1,18 @@
+package redmine_test
+
+import (
+	"os"
+	"testing"
+
+	"gorbe.io/go/redmine"
+)
+
+func TestTrackers(t *testing.T) {
+
+	v, err := redmine.NewAuthKey(os.Getenv("REDMINE_SERVER"), os.Getenv("REDMINE_KEY"), "").Trackers()
+	if err != nil {
+		t.Fatalf("%s\n", err)
+	}
+
+	t.Logf("%v\n", v)
+}
