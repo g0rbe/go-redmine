356702168d23408bfe2cc069487b481eec7c0630
Daniel Gorbe <daniel@gorbe.io>
2024-05-02 06:45:39 +0200

    update field tags

--- membership.go
+++ membership.go
@@ -1,6 +1,8 @@
 package redmine
 
 type Membership struct {
-	Project Project `json:"project"`
-	Roles   []Role  `json:"roles"`
+	Id      int      `json:"id,omitempty" yaml:"id,omitempty"`
+	Project *Project `json:"project,omitempty" yaml:"project,omitempty"`
+	User    *User    `json:"user,omitempty" yaml:"user,omitempty"`
+	Roles   []Role   `json:"roles,omitempty" yaml:"roles,omitempty"`
 }
